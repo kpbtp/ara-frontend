df301edb38173bea74907d38b678b6fdd11777a9
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-05-17 15:13:07 -0700

    switch drivers and add pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Footer from './components/Footer'
-import { useState } from 'react'
-import mockUsers from './mockUsers'
 import Signup from './components/Signup'
 import Login from './components/Login'
+import AnimeIndex from './pages/AnimeIndex'
+import AnimeShow from './pages/AnimeShow'
+import MyAnimeList from './pages/MyAnimeList'
+import MyAnimeListEdit from './pages/MyAnimeListEdit'
+import MyAnimeListShow from './pages/MyAnimeListShow'
+import MyAnimeListNew from './pages/MyAnimeListNew'
+import NotFound from './pages/NotFound'
+import mockUsers from './mockUsers'
+import mockAnime from './mockAnime'
+
 
 
 const App = () => {
 
   const [currentUser, setCurrentUser] = useState(mockUsers)
+  const [anime, setAnime] = useState(mockAnime)
 
   return(
     <>
@@ -20,7 +30,13 @@ const App = () => {
           <Route path='/' element={<Home /> }/>
           <Route path='/signup' element={<Signup />} />
           <Route path='/login' element={<Login />} />
-
+          <Route path='/animeindex' element={<AnimeIndex anime={anime}/>}/>
+          <Route path='/animeshow' element={<AnimeShow />}/>
+          <Route path='/myanimelist' element={<MyAnimeList />}/>
+          <Route path='/myanimelistedit' element={<MyAnimeListEdit />}/>
+          <Route path='/myanimelistnew' element={<MyAnimeListNew />}/>
+          <Route path='/myanimelistshow' element={<MyAnimeListShow />}/>
+          <Route path='/notfound' element={<NotFound/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/pages/MyAnimeListEdit.js
@@ -0,0 +1,12 @@
+
+
+const MyAnimeListEdit = () => {
+
+    return (
+        <>
+            <h2> Edit an anime in my anime list</h2>
+        </>
+    )
+}
+
+export default MyAnimeListEdit
