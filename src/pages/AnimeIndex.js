bc03b778fac493e166c6276cb20f955379d1f207
bjohnwell <brandonjohnwell@gmail.com>
2023-05-18 14:41:53 -0700

    index page cards added and mock data passed to cards

--- src/pages/AnimeIndex.js
+++ src/pages/AnimeIndex.js
@@ -1,11 +1,37 @@
-import { Card, CardBody, CardTitle, CardSubTitle, Button } from 'reactstrap'
-import { NavLink } from 'react-router-dom'
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  CardText,
+} from "reactstrap"
+import { NavLink } from "react-router-dom"
+import "../pages/Index.css"
 
-const AnimeIndex = ({anime}) => {
-    return (
-        <>
-            <h2>a bunch of anime</h2>
-        </>
+const AnimeIndex = ({ anime }) => {
+  return (
+    <div className="card-container">
+      {anime?.map(({name,year, synopsis, run_time, seasons, episodes, studio, genres}) => {
+        return (
+          <Card>
+          <img alt="Sample" src="https://picsum.photos/300/200" />
+          <CardBody>
+            <CardTitle tag="h5">{name}</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              {genres}
+            </CardSubtitle>
+            <CardText>
+              {synopsis}
+            </CardText>
+            <NavLink to="/animeshow/{anime.id}">
+              <Button>Button</Button>
+            </NavLink>
+          </CardBody>
+        </Card>
+        )
+          })}
+    </div>
   )
 }
 
