9e9c8bfbcfa0bd2110386f71d1f292db4512776f
bjohnwell <brandonjohnwell@gmail.com>
2023-05-19 11:06:34 -0700

    show page created, updated routes in app.js for show page, updated testign for show page added navigation from show page to my anime list.

--- src/mockAnime.js
+++ src/mockAnime.js
@@ -1,26 +1,138 @@
 
 const mockAnime = [
     {
-        id: 1,
-        name: "Attack on Titan",
-        year: "2013",
-        synopsis: "Humans fight against giant humanoid creatures called Titans.",
-        run_time: "24 minutes",
-        seasons: 4,
-        episodes: 75,
-        studio: "Wit Studio",
-        genres: "Action, Drama, Fantasy"
+      id: 1,
+      name: "Attack on Titan",
+      year: "2013",
+      synopsis: "Humans fight against giant humanoid creatures called Titans.",
+      run_time: "24 minutes",
+      seasons: 4,
+      episodes: 75,
+      studio: "Wit Studio",
+      genres: "Action, Drama, Fantasy"
     },
     {
-        id: 2,
-        name: "Your Lie in April",
-        year: "2014",
-        synopsis: "A piano prodigy meets a violinist who changes his life.",
-        run_time: "22 minutes",
-        seasons: 1,
-        episodes: 22,
-        studio: "A-1 Pictures",
-        genres: "Romance, Drama, Music"
+      id: 2,
+      name: "Your Lie in April",
+      year: "2014",
+      synopsis: "A piano prodigy meets a violinist who changes his life.",
+      run_time: "22 minutes",
+      seasons: 1,
+      episodes: 22,
+      studio: "A-1 Pictures",
+      genres: "Romance, Drama, Music"
     },
-  ]
-  export default mockAnime
+    {
+      id: 3,
+      name: "Fullmetal Alchemist: Brotherhood",
+      year: "2009",
+      synopsis: "Two brothers search for the Philosopher's Stone to restore their bodies.",
+      run_time: "24 minutes",
+      seasons: 1,
+      episodes: 64,
+      studio: "Bones",
+      genres: "Action, Adventure, Fantasy"
+    },
+    {
+      id: 4,
+      name: "Naruto",
+      year: "2002",
+      synopsis: "A young ninja seeks recognition and dreams of becoming the Hokage.",
+      run_time: "23 minutes",
+      seasons: 9,
+      episodes: 220,
+      studio: "Studio Pierrot",
+      genres: "Action, Adventure, Comedy"
+    },
+    {
+      id: 5,
+      name: "One Piece",
+      year: "1999",
+      synopsis: "A pirate sets out on an adventure to find the ultimate treasure, One Piece.",
+      run_time: "24 minutes",
+      seasons: 21,
+      episodes: 989,
+      studio: "Toei Animation",
+      genres: "Action, Adventure, Comedy"
+    },
+    {
+      id: 6,
+      name: "Death Note",
+      year: "2006",
+      synopsis: "A high school student finds a mysterious notebook that grants the power to kill.",
+      run_time: "23 minutes",
+      seasons: 1,
+      episodes: 37,
+      studio: "Madhouse",
+      genres: "Mystery, Psychological, Supernatural"
+    },
+    {
+      id: 7,
+      name: "My Hero Academia",
+      year: "2016",
+      synopsis: "A boy with no powers in a world of superheroes inherits a powerful quirk.",
+      run_time: "24 minutes",
+      seasons: 6,
+      episodes: 117,
+      studio: "Bones",
+      genres: "Action, Comedy, Superhero"
+    },
+    {
+      id: 8,
+      name: "Demon Slayer: Kimetsu no Yaiba",
+      year: "2019",
+      synopsis: "A young demon slayer seeks revenge and a cure for his sister's demonic curse.",
+      run_time: "24 minutes",
+      seasons: 1,
+      episodes: 26,
+      studio: "ufotable",
+      genres: "Action, Supernatural, Historical"
+    },
+    {
+      id: 9,
+      name: "One Punch Man",
+      year: "2015",
+      synopsis: "A superhero with unmatched strength seeks a worthy opponent.",
+      run_time: "24 minutes",
+      seasons: 2,
+      episodes: 24,
+      studio: "Madhouse",
+      genres: "Action, Comedy, Superhero"
+    },
+    {
+      id: 10,
+      name: "Steins;Gate",
+      year: "2011",
+      synopsis: "A group of friends accidentally invents a time machine and faces unintended consequences.",
+      run_time: "24 minutes",
+      seasons: 1,
+      episodes: 24,
+      studio: "White Fox",
+      genres: "Thriller, Sci-Fi, Drama"
+    },
+    {
+      id: 11,
+      name: "Cowboy Bebop",
+      year: "1998",
+      synopsis: "A bounty hunter and his crew travel through space in search of lucrative rewards.",
+      run_time: "24 minutes",
+      seasons: 1,
+      episodes: 26,
+      studio: "Sunrise",
+      genres: "Action, Sci-Fi, Space, Noir"
+    },
+    {
+      id: 12,
+      name: "Hunter x Hunter",
+      year: "2011",
+      synopsis: "A young boy sets out on a dangerous journey to become a hunter.",
+      run_time: "24 minutes",
+      seasons: 6,
+      episodes: 148,
+      studio: "Madhouse",
+      genres: "Action, Adventure, Fantasy"
+    },
+  ];
+  
+  export default mockAnime;
+  
--- src/pages/AnimeShow.js
+++ src/pages/AnimeShow.js
@@ -1,12 +1,50 @@
+import { useParams, Link } from "react-router-dom"
+import { Card, CardBody, CardImg, CardText, CardTitle, Button } from "reactstrap"
 
+const AnimeShow = ({ anime }) => {
+  const { id } = useParams()
 
-const AnimeShow = () => {
+  const selectedAnime = anime.find((anime) => anime.id == id)
 
-    return(
-        <>
-            <h1>Show the AnimeShow</h1>
-        </>
-    )
+  if (!selectedAnime) {
+    return <div>Anime not found</div>
+  }
+
+  const { name, year, synopsis, run_time, seasons, episodes, studio, genres } =
+    selectedAnime
+
+  return (
+    <div className="m-auto grid items-center grid-cols-3 justify-center">
+      <Card className="my-2">
+        <CardImg
+          alt="Card image cap"
+          src="https://picsum.photos/900/180"
+          style={{
+            height: 180,
+          }}
+          top
+          width="100%"
+        />
+        <CardBody>
+          <CardTitle tag="h5">{name}</CardTitle>
+          <CardText>{synopsis}</CardText>
+          <CardText>
+            <small className="text-muted"><ul>
+                <li>{run_time}</li>
+                <li>Seasons {seasons}</li>
+                <li>Episodes {episodes}</li>
+                <li>Studio {studio}</li>
+                <li>Genres {genres}</li>
+                <li>Year {year}</li>
+                </ul></small>
+          </CardText>
+          <Link to={`/myanimelist`}>
+                <Button>Add To My Anime List</Button>
+              </Link>
+        </CardBody>
+      </Card>
+    </div>
+  )
 }
 
 export default AnimeShow
