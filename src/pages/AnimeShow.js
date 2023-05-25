58861d30ecd23d1290a4130f9ba135b680d15f84
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-05-25 13:25:26 -0700

    animeshow page up and running

--- src/pages/AnimeShow.js
+++ src/pages/AnimeShow.js
@@ -1,45 +1,44 @@
-import React, { useState, useContext } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const AnimeShow = ({ current_user, id }) => {
-  console.log("Anime for anime show: ", id);
-  const [singleAnime, setSingleAnime] = useState('');
+  const [singleAnime, setSingleAnime] = useState(null);
 
   useEffect(() => {
-    showAnime()
+    showAnime();
   }, []);
 
   const showAnime = () => {
     fetch(`https://api.jikan.moe/v4/anime/${id}/full`)
       .then((response) => response.json())
       .then((payload) => {
-        setSingleAnime([payload.data]);
+        setSingleAnime(payload.data);
       })
       .catch((error) => console.log("Anime show errors", error));
   };
-  console.log(singleAnime)
+
+  if (!singleAnime) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
       <div>
         <div className="m-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {singleAnime.map((anime) => (
-            <div className="bg-white shadow-md relative" key={anime.mal_id}>
-              <img
-                className="w-full h-48 object-cover"
-                src={anime.images.jpg.image_url}
-                alt={anime.title}
-              />
-              <div className="p-4">
-                <h2 className="text-lg font-bold mb-2">{anime.title}</h2>
-                <h6>{anime.genres[0].name}</h6>
-                <span className="text-gray-600 mb-4">
-                  {anime.synopsis.slice(0, 150) + " . . ."}
-                </span>
-                <p className="text-gray-700 mb-5">Duration: {anime.duration}</p>
-              </div>
+          <div className="bg-white shadow-md relative" key={singleAnime.mal_id}>
+            <img
+              className="w-full h-48 object-cover"
+              src={singleAnime.images.jpg.image_url}
+              alt={singleAnime.title}
+            />
+            <div className="p-4">
+              <h2 className="text-lg font-bold mb-2">{singleAnime.title}</h2>
+              <h6>{singleAnime.genres[0].name}</h6>
+              <span className="text-gray-600 mb-4">
+                {singleAnime.synopsis.slice(0, 150) + " . . ."}
+              </span>
+              <p className="text-gray-700 mb-5">Duration: {singleAnime.duration}</p>
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </>
