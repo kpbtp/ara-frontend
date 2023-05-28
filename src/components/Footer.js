36c4991d2dd91477a54fa2a18bb2008668560114
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-05-28 05:37:51 -0700

    styled home and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import { NavLink } from "react-router-dom"
 const Footer =() => {
     return (
         <>
-            <NavLink className='nav-link text-center my-118' to='/aboutus'>ご主人様たち &#160; &#160; &#160; DeMario - Jose - Brandon - Borja &#160; &#160; &#160; みんなさんありがとうございます</NavLink>
+            <div className="max-h-full mt-30 b-0 bg-black text-white">
+            <NavLink className="fixed bottom-0 w-full p-2 m text-center justify-center bg-black text-white">ご主人様たち &#160; &#160; &#160; DeMario - Jose - Brandon - Borja &#160; &#160; &#160; みんなさんありがとうございます</NavLink>
+            </div>
         </>
        
     )
