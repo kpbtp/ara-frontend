eee15bb483822dae6a0140a4d4b8c2ced4b3bfdc
DeeMo44 <demario326@gmail.com>
2023-05-29 18:02:51 -0400

    styling and more pics for notfound. slowed the rotation for notfound

--- src/pages/AnimeIndex.js
+++ src/pages/AnimeIndex.js
@@ -34,17 +34,17 @@ const AnimeIndex = ({ fetchAnimeIndex, currentPage, setCurrentPage, loading, ani
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 m-3">
           {animeList.map((anime) => {
             const isChecked = selectedAnimeIds.includes(anime.mal_id);
 
             return (
-              <div className="bg-gray-800 rounded shadow-md" key={anime.mal_id}>
-                <img className="w-full h-48 object-cover" src={anime.images.jpg.image_url} alt={anime.title} />
+              <div className="bg-gray-800 rounded-xl shadow-md shadow-blue-500 -top-2 -left-2" key={anime.mal_id}>
+                <img className="w-full rounded-t-xl" src={anime.images.jpg.image_url} alt={anime.title} />
                 <div className="p-4">
-                  <h2 className="text-lg font-bold mb-2">{anime.title}</h2>
-                  <span className="text-gray-500 mb-4">{anime.synopsis.slice(0, 150)}</span>
-                  <p className="text-gray-500">Duration: {anime.duration}</p>
+                  <h2 className="text-sm font-bold mb-2">{anime.title}</h2>
+                  <span className="text-xs text-gray-500 mb-4">{anime.synopsis.slice(0, 150)+'...'}</span>
+                  <p className="text-xs text-gray-500">Duration: {anime.duration}</p>
                   {currentUser && (
                     <React.Fragment key={anime.mal_id}>
                       <Input
