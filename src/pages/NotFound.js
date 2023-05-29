144e6e29f316d2066a89184f181109defdc6c759
DeeMo44 <demario326@gmail.com>
2023-05-29 10:57:58 -0400

    added random picture function and more pictures. fixed movement of mouse controls. added 2 button, one to go to prvious page and the other to go home.

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,11 @@
 import * as THREE from "three";
 import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
   const canvasRef = useRef(null);
   const mouseRef = useRef({ x: 0, y: 0 });
+  const rotationRef = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -13,14 +15,6 @@ const NotFound = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(pixelRatio);
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load(
-      "/anime_art_style_humanoid-cat-pirate_navigating_thr.jpg"
-    );
-    texture.minFilter = THREE.LinearFilter; // Set texture minFilter to LinearFilter for improved image quality
-
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
       75,
@@ -28,10 +22,19 @@ const NotFound = () => {
       0.1,
       1000
     );
-    camera.position.set(0, 10, 0);
+    camera.position.set(0, 0, 0);
 
     const geometry = new THREE.SphereGeometry(500, 60, 40);
     geometry.scale(-1, 1, 1);
+
+    //choose random picture
+    const loader = new THREE.TextureLoader();
+    function randomNum(num) {
+      return Math.floor(Math.random() * num) + 1;
+    }
+    //change num for the total number of pics
+    const texture = loader.load(`/pic${randomNum(4)}.jpg`);
+    texture.minFilter = THREE.LinearFilter;
 
     const material = new THREE.MeshBasicMaterial({ map: texture });
 
@@ -46,30 +49,37 @@ const NotFound = () => {
     function animate() {
       requestAnimationFrame(animate);
 
-      //up and down linear interpolation on  mouse move
-      const targetRotationX = mouseRef.current.y * 0.2;
-      scene.rotation.x += (-targetRotationX - scene.rotation.x) * 0.05;
+      const targetRotationX = mouseRef.current.y * 0.5;
+      const targetRotationY = mouseRef.current.x * 0.5;
 
-      //left and right linear interpolation on mouse move
-      const targetRotationY = mouseRef.current.x * 0.2;
-      scene.rotation.y += (targetRotationY - scene.rotation.y) * 0.05;
+      rotationRef.current.x += (targetRotationX - rotationRef.current.x) * 0.05;
+      rotationRef.current.y += (targetRotationY - rotationRef.current.y) * 0.05;
 
-      //rotates the screen
-      camera.rotation.y += -0.003;
+      camera.rotation.x = rotationRef.current.x;
+      camera.rotation.y = -rotationRef.current.y;
+
+      scene.rotation.y += 0.003;
 
       renderer.render(scene, camera);
     }
 
     animate();
 
-    // Event listener for mouse movement
     window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      // Clean up event listener
       window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
+
+  //handleclicks for navigation
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+  const handleGoHome = () => {
+    navigate("/");
+  };
 
   return (
     <div style={{ position: "fixed", width: "100vw", height: "100vh" }}>
@@ -84,8 +94,18 @@ const NotFound = () => {
       >
         <h1>404</h1>
         <p>Sorry Anime Not Found...</p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md">
-          Return from whence you came
+        <button
+          onClick={() => handleGoBack()}
+          className="bg-blue-500 hover:bg-purple-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md transform transition-all duration-500 ease-in-out hover:scale-110"
+        >
+          Go Back
+        </button>
+
+        <button
+          onClick={() => handleGoHome()}
+          className="bg-blue-500 hover:bg-pink-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md bg-gradient-to-r from-purple-500 to-blue-500 hover:from-pink-500 hover:to-purple-500 transition-all duration-500 ease-in-out"
+        >
+          Go Home
         </button>
       </div>
     </div>
