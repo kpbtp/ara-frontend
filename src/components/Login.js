8290792064f74b73b0d73a1004896561ed5ce177
DeeMo44 <demario326@gmail.com>
2023-05-29 22:15:22 -0400

    organized the structure of the login page. not fina product-subject to change.

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,26 +21,49 @@ const Login = ({ login }) => {
   };
 
   return (
-    <section className='max-h-full h-screen bg-gradient-to-b p-1 from-gray-950 via-gray-800 to-gray-800'>
+    <section className="max-h-full h-screen bg-gradient-to-b p-1 from-gray-950 via-gray-800 to-gray-800">
       <div className="shadow-xl shadow-rose-500 m-20 bg-rose-600  p-10 rounded-xl my-20 flex-col">
         <div className="bg-gray-600  p-3 rounded-lg">
           <h2 className="text-3xl text-center">Login</h2>
           <div className="flex items-center leading-8 justify-center">
             <form className="px-3" ref={formRef} onSubmit={handleSubmit}>
-              Email: <input type="email" name="email" placeholder="email" />
+
+              <div className="grid grid-cols-1">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="rounded mb-2 mt-3 px-1 justify-center"
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="rounded px-1"
+              />
+              </div>
               <br />
-              Password:{" "}
-              <input type="password" name="password" placeholder="password" />
+              <div className="flex justify-center">
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="bg-pink-800 hover:bg-pink-600 text-white font-bold  px-2 rounded"
+                />
+              </div>
               <br />
-              <input type="submit" value="Submit" />
-              <br />
-            Already registered, <a href="/login">Login</a> here.
-             </form>
+              Already registered?{" "}
+              <a
+                href="/signup"
+                className="text-white no-underline hover:text-gray-300"
+              >
+                <span className="hover:text-pink-400">Signup</span>
+              </a>{" "}
+              here!
+            </form>
           </div>
         </div>
       </div>
     </section>
-
   );
 };
 
