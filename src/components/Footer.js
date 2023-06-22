f54d3e36cc8204c353b66abdeed1c28a785d3aef
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-06-22 08:12:45 -0700

    ui added new styles to home page and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom"
 const Footer =() => {
     return (
         <>
-            <div className="max-h-full mt-30 b-0 bg-gray-800 text-white">
-            <NavLink className="fixed bottom-0 w-full p-4 m text-center justify-center bg-gray-800 text-white" to='/aboutus'>ご主人様たち &#160; &#160; &#160; DeMario - Jose - Brandon - Borja &#160; &#160; &#160; みんなさんありがとうございます</NavLink>
+            <div className="max-h-full flex justify-center mt-30 b-0 bg-gray-900 text-white">
+            <NavLink className=" p-4 m text-center justify-center text-white" to='/aboutus'>ご主人様たち &#160; &#160; &#160; DeMario - Jose - Brandon - Borja &#160; &#160; &#160; みんなさんありがとうございます</NavLink>
             </div>
         </>
        
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,16 +35,28 @@ const Home = (args) => {
       src: carouselImage ? carouselImage[1].images.jpg.image_url : '',
       altText: 'Man with a horse',
       key: 1,
+      title: carouselImage ? carouselImage[1].title : '', 
+      genres: carouselImage ? carouselImage[1].genres : '',
+      year: carouselImage ? carouselImage[1].year : '',
+      synopsis: carouselImage ? carouselImage[1].synopsis : '',
     },
     {
       src: carouselImage ? carouselImage[2].images.jpg.image_url : '',
       altText: 'Man with a horse',
       key: 2,
+      title: carouselImage ? carouselImage[2].title : '',
+      genres: carouselImage ? carouselImage[2].genres : '',
+      year: carouselImage ? carouselImage[2].year : '',
+      synopsis: carouselImage ? carouselImage[2].synopsis : '',
     },
     {
       src: carouselImage ? carouselImage[3].images.jpg.image_url : '',
       altText: 'Man with a horse',
       key: 3,
+      title: carouselImage ? carouselImage[3].title : '',
+      genres: carouselImage ? carouselImage[3].genres : '',
+      year: carouselImage ? carouselImage[3].year : '',
+      synopsis: carouselImage ? carouselImage[3].synopsis : '',
     },
   ];
 
@@ -70,18 +82,40 @@ const Home = (args) => {
       key={item.key}
       onExiting={() => setAnimating(true)}
       onExited={() => setAnimating(false)}
+      className='px-3 pb-10 bg-gradient-to-r from-black to-gray-900 rounded-xl'
     >
-     <div className="d-flex justify-content-center">
+     <div className="m-4 rounded-lg flex justify-content-center">
         <img src={item.src} alt={item.altText} className="rounded-xl" />
+      </div>
+      <div className="p-3 rounded-lg shadow-xl shadow-black columns-1 bg-gradient-to-l from-slate-900 via-slate-800 to-slate-900 ">
+        <h3 className=" px-10 m-1 text-rose-500 font-medium text-center">
+          {item.title}
+        </h3>
+        
+        <p className="text-gray-300  px-10 m-1 text-sm text-center">{item.year}</p>
+        <p className="text-gray-300  px-10 m-1 text-sm text-center">{item.synopsis}</p>
       </div>
     </CarouselItem>
   ));
 
   return (
     <>
-      <div className='vh-100 bg-gradient-to-b p-1 from-gray-950 via-gray-800 to-gray-800 text-white'>
-        <section>
-          <div className='p-10'>
+      <div className=' bg-gradient-to-br p-1  from-gray-950 via-gray-800 to-gray-800 text-white'>
+      <section className='min-h-auto m-5 px-10 '>
+          <h2 className='text-5xl py-2 m-1 text-rose-500 font-medium text-center'>
+            Welcome to Ara
+          </h2>
+          <p className='text-md px-10 py-2 mx-20 my-4 rounded-lg leading-8 text-center bg-slate-500/75 '>
+            Stay up-to-date with the latest additions to our library. From
+            brand-new seasons of your <span className='text-rose-500'>favorite shows</span> to highly anticipated
+            anime adaptations, our new releases section showcases the freshest
+            content that will keep you on the edge of your seat. Get a sneak
+            peek into exciting stories and follow your beloved characters' new
+            adventures.
+          </p>
+        </section>
+        <section className='min-h-auto'>
+          <div className='px-10 pb-10'>
             <Carousel
               style={{ alignItems: 'center' }}
               activeIndex={activeIndex}
@@ -112,19 +146,7 @@ const Home = (args) => {
             </Carousel>
           </div>
         </section>
-        <section>
-          <h2 className='text-5xl py-2 m-1 text-rose-500 font-medium text-center'>
-            Ara is the Best
-          </h2>
-          <p className='text-md p-10 m-20 rounded-lg leading-8 text-center bg-slate-500/75 '>
-            Stay up-to-date with the latest additions to our library. From
-            brand-new seasons of your <span className='text-rose-500'>favorite shows</span> to highly anticipated
-            anime adaptations, our new releases section showcases the freshest
-            content that will keep you on the edge of your seat. Get a sneak
-            peek into exciting stories and follow your beloved characters' new
-            adventures.
-          </p>
-        </section>
+        
         
       </div>
     </>
